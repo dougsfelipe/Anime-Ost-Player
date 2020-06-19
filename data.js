cb8cc500d2b9931db50d0296994faee1db040488
dougsfelipe <dfcs@cin.ufpe.br>
2020-06-19 01:17:22 -0300

    adicionando musicas

--- data.js
+++ data.js
@@ -1,21 +1,88 @@
 export default [
   {
     title:
-      "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
-    artist: "Diego Fernandes",
-    cover: "como-comecei.jpg",
-    file: "como-comecei.mp3"
+      "A Cruel Angel's Thesis",
+    artist: "Yoko Takahashi.",
+    cover: "/covers/evangelion.jpg",
+    file: "/songs/evangelion1.mp3"
   },
   {
-    title: "5 dicas para uma carreira sólida como programador",
-    artist: "Diego Fernandes",
-    cover: "5-dicas-para-uma-carreira-solida-como-programador.jpg",
-    file: "5-dicas-para-uma-carreira-solida-como-programador.mp3"
+    title: "TANK!",
+    artist: "Yoko Kanno & The Seatbelts",
+    cover: "/covers/cb.jpg",
+    file: "/songs/cowboyBeboop1.mp3"
   },
   {
-    title: "Júnior Pleno ou Sênior, qual a diferença?",
-    artist: "Diego Fernandes",
-    cover: "junior-pleno-ou-senior-qual-a-diferenca.jpg",
-    file: "junior-pleno-ou-senior-qual-a-diferenca.mp3"
+    title: "Que o Seu Sonho Se Realize",
+    artist: "Shaman King",
+    cover: "/covers/sk.jpg",
+    file: "/songs/shamanKing2.mp3"
+  },
+  {
+    title: "RED SWAN",
+    artist: "Yoshiki featuring Hyde",
+    cover: "/covers/at.jpg",
+    file: "/songs/atackOnTitan3.mp3"
+  },
+  {
+    title: "Stray",
+    artist: "Steve Conte",
+    cover: "/covers/0.jpg",
+    file: "/songs/wolfsRain2.mp3"
+  },
+  {
+    title: "Flyers",
+    artist: "Bradio",
+    cover: "/covers/dp.jpg",
+    file: "/songs/deathParade.mp3"
+  },
+  {
+    title: "Call me Call me",
+    artist: "Yoko Kanno & The Seatbelts",
+    cover: "/covers/cb2.jpg",
+    file: "/songs/cowboyBeboop2.mp3"
+  },
+  {
+    title: "Melissa",
+    artist: "Porn Graffiti",
+    cover: "/covers/fma.jpg",
+    file: "/songs/FMA1.mp3"
+  },
+  {
+    title: "Sign",
+    artist: "Flow!",
+    cover: "/covers/Sign.png",
+    file: "/songs/naruto6.mp3"
+  },
+  {
+    title: "PapperMoon",
+    artist: "Tomoko Kawase",
+    cover: "/covers/se.jpg",
+    file: "/songs/soulEater2.mp3"
+  },
+  {
+    title: "Touch Off",
+    artist: "UVERworld",
+    cover: "/covers/tpn.png",
+    file: "/songs/TPN.mp3"
+  },
+  {
+    title: "Rinne Rondo",
+    artist: "On/Off",
+    cover: "/covers/vk.jpg",
+    file: "/songs/vampireKnight.mp3"
+  },
+  {
+    title: "Gravity",
+    artist: "Maaya Sakamoto",
+    cover: "/covers/wr2.gif",
+    file: "/songs/wolfsRain1.mp3"
+  },
+  {
+    title: "Rinbu Revolution",
+    artist: "Masami Okui",
+    cover: "/covers/utena.jpg",
+    file: "/songs/utena.mp3"
   }
 ];
+
